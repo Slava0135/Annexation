e89c8504009002c36cd982c209028f7abdcd4c25
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-26 18:28:27 +0300

    pause fix

--- src/main/java/Annexation.java
+++ src/main/java/Annexation.java
@@ -41,50 +41,52 @@ public class Annexation extends Plugin {
 
         Timer.schedule(() -> {
 
-            Seq<TeamData> teams = Vars.state.teams.present;
-            for (TeamData team : teams) {
-                int scoreIncrease = 0;
-                if (team.team == Team.derelict) continue;
-                for (Building core : team.cores) {
-                    scoreIncrease += core.block.size;
+            if (!Vars.state.serverPaused) {
+                Seq<TeamData> teams = Vars.state.teams.present;
+                for (TeamData team : teams) {
+                    int scoreIncrease = 0;
+                    if (team.team == Team.derelict) continue;
+                    for (Building core : team.cores) {
+                        scoreIncrease += core.block.size;
+                    }
+                    lastIncrease.put(team.team, scoreIncrease);
+                    scores.put(team.team, scores.getOrDefault(team.team, 0) + scoreIncrease);
                 }
-                lastIncrease.put(team.team, scoreIncrease);
-                scores.put(team.team, scores.getOrDefault(team.team, 0) + scoreIncrease);
-            }
 
-            Map.Entry<Team, Integer> maxScore = null;
-            for (Map.Entry<Team, Integer> score : scores.entrySet()) {
-                if (maxScore == null || score.getValue() > maxScore.getValue()) {
-                    maxScore = score;
+                Map.Entry<Team, Integer> maxScore = null;
+                for (Map.Entry<Team, Integer> score : scores.entrySet()) {
+                    if (maxScore == null || score.getValue() > maxScore.getValue()) {
+                        maxScore = score;
+                    }
                 }
-            }
 
-            if (maxScore != null) {
-                int bestScore = maxScore.getValue();
-                if (bestScore > winScore) {
-                    Team winner = maxScore.getKey();
-                    Events.fire(new EventType.GameOverEvent(winner));
+                if (maxScore != null) {
+                    int bestScore = maxScore.getValue();
+                    if (bestScore > winScore) {
+                        Team winner = maxScore.getKey();
+                        Events.fire(new EventType.GameOverEvent(winner));
+                    }
                 }
+
+                scores.entrySet().removeIf(entry -> !entry.getKey().active());
+                lastIncrease.entrySet().removeIf(entry -> !entry.getKey().active());
+
+                List<Map.Entry<Team, Integer>> list = new ArrayList<>(scores.entrySet());
+                list.sort(Map.Entry.comparingByValue());
+                reverse(list);
+
+                String progress = "winscore is " + winScore;
+
+                int count = 0;
+                for (Map.Entry<Team, Integer> entry : list) {
+                    count++;
+                    if(count > topLength) break;
+                    Team team = entry.getKey();
+                    int score = entry.getValue();
+                    progress += "\n#" + count + " [#" + team.color.toString() + "]" + team.name + " : " + score + " + " + lastIncrease.getOrDefault(team, 0) + "[]";
+                }
+                Call.infoPopup(progress, updateInterval, Align.bottom, 0, 0, 0, 0);
             }
-
-            scores.entrySet().removeIf(entry -> !entry.getKey().active());
-            lastIncrease.entrySet().removeIf(entry -> !entry.getKey().active());
-
-            List<Map.Entry<Team, Integer>> list = new ArrayList<>(scores.entrySet());
-            list.sort(Map.Entry.comparingByValue());
-            reverse(list);
-
-            String progress = "winscore is " + winScore;
-
-            int count = 0;
-            for (Map.Entry<Team, Integer> entry : list) {
-                count++;
-                if(count > topLength) break;
-                Team team = entry.getKey();
-                int score = entry.getValue();
-                progress += "\n#" + count + " [#" + team.color.toString() + "]" + team.name + " : " + score + " + " + lastIncrease.getOrDefault(team, 0) + "[]";
-            }
-            Call.infoPopup(progress, updateInterval, Align.bottom, 0, 0, 0, 0);
 
         }, 0, updateInterval);
 
